34ba3411afff241c6f429804f1f74b4330979790
Dobrovsky <miron.dobrovsky.work@gmail.com>
2024-07-25 20:09:05 +0300

    Initial Commit

---
+++ Ex3/Ex3App.swift
@@ -0,0 +1,17 @@
+//
+//  Ex3App.swift
+//  Ex3
+//
+//  Created by Dobrovsky on 25.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Ex3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
